20dd1d843699d97f31d97bf4014bb4cfdb52e734
Danny <dannyvankooten@gmail.com>
2017-08-30 14:00:50 +0200

    use pageYOffset instead of scrollY for IE compat. closes #107

--- dist/boxzilla.js
+++ dist/boxzilla.js
@@ -1126,7 +1126,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         // check triggerHeight criteria for all boxes
         function checkHeightCriteria() {
 
-            var scrollY = scrollElement.hasOwnProperty('scrollY') ? scrollElement.scrollY : scrollElement.scrollTop;
+            var scrollY = scrollElement.hasOwnProperty('pageYOffset') ? scrollElement.pageYOffset : scrollElement.scrollTop;
             scrollY = scrollY + window.innerHeight * 0.9;
 
             boxes.forEach(function (box) {
